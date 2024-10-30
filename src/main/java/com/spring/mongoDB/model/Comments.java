354f9ae3943e1c2cbc3dc61eefdb489df27e1ab7
shamimAhamed <shammeedd@gmail.com>
2024-10-30 23:14:23 +0600

    date is added

--- src/main/java/com/spring/mongoDB/model/Comments.java
+++ src/main/java/com/spring/mongoDB/model/Comments.java
@@ -1,9 +1,12 @@
 package com.spring.mongoDB.model;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,6 +25,8 @@ public class Comments {
 	String author;
 	List<?> comments;
 	Metadata metadata;
+	@DateTimeFormat(iso=DateTimeFormat.ISO.DATE_TIME)
+	Date createdDate;
 
 	
 	
